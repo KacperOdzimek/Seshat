efcc76620d141a8da021388e6cec1696a320406e
Kacper Odzimek <kacper-odzimek@wp.pl>
2026-07-20 18:52:41 +0200

    Format changes

--- include/seshat/seshat.h
+++ include/seshat/seshat.h
@@ -6,76 +6,9 @@
 #define SESHAT_H
 
 #include <stdint.h>
+#include <stdio.h>
 
-// Basic
 
-typedef enum sht_data_type {
-    sht_data_type_archive   = 0,
-    sht_data_type_int64     = 1,
-    sht_data_type_float64   = 2,
-    sht_data_type_utf8      = 3,
-    sht_data_type_binary    = 4 
-} sht_data_type;
-
-// Archive
-
-typedef struct sht_archive_create_info {
-    uint64_t    file_bytes; // If non-zero reads archive from file, else creates a new blank one
-    const void* file_data;  // If file_bytes non zero must be valid archive data
-} sht_archive_create_info;
-
-typedef struct sht_archive sht_archive;
-sht_archive* sht_create_archive(const sht_archive_create_info* info);
-void sht_free_archive(sht_archive*);
-
-// Archive iteration
-
-uint32_t sht_archive_query_index_entries_count(sht_archive*);           // Count of entries in index
-uint32_t sht_archive_query_index_position(sht_archive*, const char*);   // Returns given entry position, on fail returns count of entries
-
-// Archive queries
-
-const char*     sht_archive_query_name      (sht_archive*, uint32_t);   // Returns given entry name
-sht_data_type   sht_archive_query_data_type (sht_archive*, uint32_t);   // Returns data type of given entry
-int             sht_archive_query_compressed(sht_archive*, uint32_t);   // Returns whether given entry data is compressed
-
-// Archive value queries
-// All of operations return non-zero at success
-// Only then value at *target is valid
-// Data is automatically decompressed
-
-int sht_archive_query_value_archive(sht_archive*, uint32_t, sht_archive** target);
-
-int sht_archive_query_value_int64  (sht_archive*, uint32_t, int64_t*      target);    
-int sht_archive_query_value_float64(sht_archive*, uint32_t, double*       target);
-
-int sht_archive_query_value_utf_8_copy (sht_archive*, uint32_t, uint64_t* bytes, void** begin);  // Copies data - caller must free begin!
-int sht_archive_query_value_binary_copy(sht_archive*, uint32_t, uint64_t* bytes, void** begin);  // Copies data - caller must free begin!
-
-int sht_archive_query_value_utf_8_view(sht_archive*,  uint32_t, uint64_t* bytes, const void** begin);   // Allows read-only view into file data
-int sht_archive_query_value_binary_view(sht_archive*, uint32_t, uint64_t* bytes, const void** begin);   // Allows read-only view into file data
-
-// Archive updates
-// All of operations return non-zero at success
-// If previous value was an archive/utf8/binary it will be freed
-// The given memory blocks are claimed by archive
-
-int sht_archive_update_value_archive(sht_archive*, uint32_t, sht_archive* value);
-
-int sht_archive_update_value_int64  (sht_archive*, uint32_t, int64_t value);
-int sht_archive_update_value_float64(sht_archive*, uint32_t, double value);
-
-int sht_archive_update_value_utf8   (sht_archive*, uint32_t, uint64_t bytes, const void* data);
-int sht_archive_update_value_binary (sht_archive*, uint32_t, uint64_t bytes, const void* data);
-
-int sht_archive_update_compress     (sht_archive*, uint32_t, int compress);
-
-// Archive addition
-// All of operations return non-zero at success
-// Those operations invalidates iterators
-
-int sht_archive_add_entries   (sht_archive* archive, uint32_t entries_count, const char** names);
-int sht_archive_remove_entries(sht_archive* archive, uint32_t entries_count, const char** names);
 
 #endif // SESHAT_H
 
